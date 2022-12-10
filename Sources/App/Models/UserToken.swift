5386907e52f06b2ddea33feb19d3a5ec2d805df4
Turner Eison <turner@turnereison.com>
2022-12-10 01:20:58 -0500

    silly fix?

--- Sources/App/Models/UserToken.swift
+++ Sources/App/Models/UserToken.swift
@@ -38,7 +38,7 @@ extension UserToken: ModelTokenAuthenticatable {
     static var userKey = \UserToken.$user
     
     var isValid: Bool {
-        return expirationDate > .now
+        return expirationDate > Date()
     }
 }
 
