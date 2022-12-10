6e8835f13006536920ba3abba3f95ac4f3a05efe
Turner Eison <turner@turnereison.com>
2022-12-10 14:46:43 -0500

    added silly shutdown route

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -6,4 +6,12 @@ func routes(_ app: Application) throws {
     try app.register(collection: LocationRoutes())
     try app.register(collection: TagRoutes())
     try app.register(collection: SessionRoutes())
+    
+    app.post("shutdown") { req -> HTTPStatus in
+        Task.detached {
+            try await Task.sleep(nanoseconds: 1_000_000_000) // 1 second
+            fatalError()
+        }
+        return .ok
+    }
 }
